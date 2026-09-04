0f73f639052ff6cf25609ce77baa094fbf398496
longlongzzl <61743986+longlongzzl@users.noreply.github.com>
2026-09-05 00:28:47 +0800

    feat: export unified scenario APIs

---
+++ rm75_app/scenarios/__init__.py
@@ -0,0 +1,65 @@
+"""Unified scenario layer for sorting, magnetic assembly, and Push-T."""
+
+from .contracts import (
+    ExecutionMode,
+    PreparedStep,
+    ProgramStep,
+    ScenarioKind,
+    ScenarioObservation,
+    ScenarioProgram,
+    ScenarioRunReport,
+    SceneStamp,
+    StepExecutionResult,
+    StepStatus,
+    stable_fingerprint,
+)
+from .pickplace_program import (
+    AtomBoundaryCommand,
+    CompiledPickPlaceAtom,
+    CompiledPickPlaceProgram,
+    GripperCommand,
+    PickPlaceCompilationResult,
+    PickPlaceExecutionReport,
+    PickPlaceProgramCompiler,
+    PickPlaceProgramExecutor,
+    TrajectoryCommand,
+)
+from .program_runner import ScenarioProgramRunner
+from .sorting import (
+    PreparedSortingProgram,
+    SortingAssignment,
+    SortingPlanCompiler,
+    SortingRequest,
+    SortingSystem,
+    SortingTarget,
+)
+
+__all__ = [
+    "AtomBoundaryCommand",
+    "CompiledPickPlaceAtom",
+    "CompiledPickPlaceProgram",
+    "ExecutionMode",
+    "GripperCommand",
+    "PickPlaceCompilationResult",
+    "PickPlaceExecutionReport",
+    "PickPlaceProgramCompiler",
+    "PickPlaceProgramExecutor",
+    "PreparedSortingProgram",
+    "PreparedStep",
+    "ProgramStep",
+    "ScenarioKind",
+    "ScenarioObservation",
+    "ScenarioProgram",
+    "ScenarioProgramRunner",
+    "ScenarioRunReport",
+    "SceneStamp",
+    "SortingAssignment",
+    "SortingPlanCompiler",
+    "SortingRequest",
+    "SortingSystem",
+    "SortingTarget",
+    "StepExecutionResult",
+    "StepStatus",
+    "TrajectoryCommand",
+    "stable_fingerprint",
+]
